2884b1eed52b4cf0818bff6de4862b39ba44e9b7
PilipDagh <mcguffin221@gmail.com>
2026-05-18 08:35:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,16 +12,12 @@ const MAX_PLAYERS = 50;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// --- 5 ITEM PRESETS (Granny Style) ---
+// --- ITEM PRESETS (Granny Style) ---
 const PRESETS = [
-    { hammer: {x: 15, y: -3.5, z: -25}, pliers: {x: 12, y: 0.5, z: 5}, master_key: {x: 0, y: 8.5, z: 0}, weapons_key: {x: -3, y: 0.5, z: 8}, gas_can: {x: 25, y: -3.5, z: -25}, car_key: {x: 5, y: 8.5, z: 5}, battery: {x: 10, y: 0.5, z: -5} },
-    { hammer: {x: 0, y: 8.5, z: 0}, pliers: {x: 15, y: -3.5, z: -25}, master_key: {x: 12, y: 0.5, z: 5}, weapons_key: {x: 25, y: -3.5, z: -25}, gas_can: {x: -3, y: 0.5, z: 8}, car_key: {x: 10, y: 0.5, z: -5}, battery: {x: 5, y: 8.5, z: 5} },
-    { hammer: {x: 12, y: 0.5, z: 5}, pliers: {x: 0, y: 8.5, z: 0}, master_key: {x: 15, y: -3.5, z: -25}, weapons_key: {x: 5, y: 8.5, z: 5}, gas_can: {x: 10, y: 0.5, z: -5}, car_key: {x: 25, y: -3.5, z: -25}, battery: {x: -3, y: 0.5, z: 8} },
-    { hammer: {x: 25, y: -3.5, z: -25}, pliers: {x: -3, y: 0.5, z: 8}, master_key: {x: 10, y: 0.5, z: -5}, weapons_key: {x: 12, y: 0.5, z: 5}, gas_can: {x: 5, y: 8.5, z: 5}, car_key: {x: 15, y: -3.5, z: -25}, battery: {x: 0, y: 8.5, z: 0} },
-    { hammer: {x: 5, y: 8.5, z: 5}, pliers: {x: 10, y: 0.5, z: -5}, master_key: {x: -3, y: 0.5, z: 8}, weapons_key: {x: 0, y: 8.5, z: 0}, gas_can: {x: 15, y: -3.5, z: -25}, car_key: {x: 12, y: 0.5, z: 5}, battery: {x: 25, y: -3.5, z: -25} }
+    { hammer: {x: 15, y: -3.5, z: -25}, pliers: {x: 12, y: 0.5, z: 5}, master_key: {x: 0, y: 8.5, z: 0}, weapons_key: {x: -3, y: 0.5, z: 8}, gas_can: {x: 25, y: -3.5, z: -25}, car_key: {x: 5, y: 8.5, z: 5}, battery: {x: 10, y: 0.5, z: -5} }
 ];
 
-let currentPresetIndex = Math.floor(Math.random() * PRESETS.length);
+let currentPresetIndex = 0;
 let players = {};
 let gameState = { items: {}, puzzles: {}, carParts: { gas: false, battery: false, key: false } };
 
@@ -32,12 +28,20 @@ function loadPreset() {
         'pliers': { pos: p.pliers, holder: null, visible: true },
         'master_key': { pos: p.master_key, holder: null, visible: true },
         'weapons_key': { pos: p.weapons_key, holder: null, visible: true },
-        'crossbow': { pos: {x: 22, y: 0.5, z: 8}, holder: null, visible: false },
+        'crossbow': { pos: {x: 22, y: 1.5, z: 8}, holder: null, visible: false }, // Inside wall box
+        'arrow_1': { pos: {x: 22.2, y: 1.5, z: 8}, holder: null, visible: false },
+        'arrow_2': { pos: {x: 22.4, y: 1.5, z: 8}, holder: null, visible: false },
+        'arrow_3': { pos: {x: 22.6, y: 1.5, z: 8}, holder: null, visible: false },
         'gas_can': { pos: p.gas_can, holder: null, visible: true },
         'car_key': { pos: p.car_key, holder: null, visible: true },
         'battery': { pos: p.battery, holder: null, visible: true }
     };
-    gameState.puzzles = { 'barricade': { solved: false }, 'circuitBox': { solved: false }, 'mainDoor': { solved: false }, 'weaponsCase': { solved: false } };
+    gameState.puzzles = { 
+        'barricade': { solved: false }, 
+        'circuitBox': { solved: false }, 
+        'mainDoor': { solved: false }, 
+        'weaponsCase': { solved: false, isOpen: false } // Wall mounted box
+    };
     gameState.carParts = { gas: false, battery: false, key: false };
 }
 loadPreset();
@@ -49,13 +53,11 @@ io.on('connection', (socket) => {
         return;
     }
 
-    // 0.67% Chance for Domain Expansion!
     const hasDomain = Math.random() < 0.0067;
 
     players[socket.id] = { 
         id: socket.id, username: 'Guest-' + Math.floor(Math.random() * 9000000), 
-        pos: { x: 0, y: 9.7, z: 0 }, // Spawn UPSTAIRS
-        rot: 0, lives: 5, isDead: false, isHiding: false, hasDomain: hasDomain,
+        pos: { x: 0, y: 9.7, z: 0 }, rot: 0, lives: 5, isDead: false, isHiding: false, isCrouching: false, hasDomain: hasDomain,
         customization: { hair: '#000000', clothes: '#2244aa', skin: '#ffccaa', pants: '#111111', shoes: '#333333' }
     };
 
@@ -63,39 +65,21 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('playerJoined', players[socket.id]);
 
     socket.on('setUsername', (name) => { if (name.trim().length > 0) players[socket.id].username = name.trim(); });
-
-    // Customization Sync
-    socket.on('updateCustomization', (colors) => {
-        if (players[socket.id]) {
-            players[socket.id].customization = colors;
-            io.emit('playerCustomized', { id: socket.id, colors: colors });
-        }
-    });
+    socket.on('updateCustomization', (colors) => { if (players[socket.id]) { players[socket.id].customization = colors; io.emit('playerCustomized', { id: socket.id, colors: colors }); } });
 
     socket.on('move', (data) => {
         if (players[socket.id] && !players[socket.id].isDead) {
             players[socket.id].pos = data.pos;
             players[socket.id].rot = data.rot;
-            socket.broadcast.emit('playerMoved', { id: socket.id, pos: data.pos, rot: data.rot });
+            players[socket.id].isCrouching = data.isCrouching;
+            socket.broadcast.emit('playerMoved', { id: socket.id, pos: data.pos, rot: data.rot, isCrouching: data.isCrouching });
         }
     });
 
-    socket.on('setHiding', (isHiding) => {
-        if (players[socket.id]) {
-            players[socket.id].isHiding = isHiding;
-            socket.broadcast.emit('playerHiding', { id: socket.id, isHiding: isHiding });
-        }
-    });
-
+    socket.on('setHiding', (isHiding) => { if (players[socket.id]) { players[socket.id].isHiding = isHiding; socket.broadcast.emit('playerHiding', { id: socket.id, isHiding: isHiding }); } });
     socket.on('makeNoise', (pos) => io.emit('noiseMade', pos));
     socket.on('shootTungTung', () => io.emit('tungTungKnockedOut'));
-
-    // Domain Expansion Sync
-    socket.on('activateDomain', (pos) => {
-        if (players[socket.id] && players[socket.id].hasDomain) {
-            io.emit('domainActivated', { id: socket.id, pos: pos });
-        }
-    });
+    socket.on('activateDomain', (pos) => { if (players[socket.id] && players[socket.id].hasDomain) io.emit('domainActivated', { id: socket.id, pos: pos }); });
 
     socket.on('itemAction', (data) => {
         const item = gameState.items[data.itemId];
@@ -110,33 +94,33 @@ io.on('connection', (socket) => {
         if (gameState.puzzles[data.obstacleId]) {
             gameState.puzzles[data.obstacleId].solved = true;
             io.emit('puzzleUpdate', { obstacleId: data.obstacleId });
+            
+            // Open weapons case and reveal crossbow + arrows
             if (data.obstacleId === 'weaponsCase') {
+                gameState.puzzles['weaponsCase'].isOpen = true;
                 gameState.items['crossbow'].visible = true;
+                gameState.items['arrow_1'].visible = true;
+                gameState.items['arrow_2'].visible = true;
+                gameState.items['arrow_3'].visible = true;
                 io.emit('itemUpdate', { itemId: 'crossbow', itemState: gameState.items['crossbow'] });
+                io.emit('itemUpdate', { itemId: 'arrow_1', itemState: gameState.items['arrow_1'] });
+                io.emit('itemUpdate', { itemId: 'arrow_2', itemState: gameState.items['arrow_2'] });
+                io.emit('itemUpdate', { itemId: 'arrow_3', itemState: gameState.items['arrow_3'] });
             }
-            if (data.obstacleId === 'mainDoor') {
-                io.emit('gameWon', { winner: players[socket.id].username, type: 'Front Door' });
-                resetServerState();
-            }
+            if (data.obstacleId === 'mainDoor') { io.emit('gameWon', { winner: players[socket.id].username, type: 'Front Door' }); resetServerState(); }
         }
     });
 
-    // Car Escape Logic
     socket.on('carPartAdded', (part) => {
-        gameState.carParts[part] = true;
-        io.emit('carUpdate', gameState.carParts);
-        if (gameState.carParts.gas && gameState.carParts.battery && gameState.carParts.key) {
-            io.emit('gameWon', { winner: players[socket.id].username, type: 'Car' });
-            resetServerState();
-        }
+        gameState.carParts[part] = true; io.emit('carUpdate', gameState.carParts);
+        if (gameState.carParts.gas && gameState.carParts.battery && gameState.carParts.key) { io.emit('gameWon', { winner: players[socket.id].username, type: 'Car' }); resetServerState(); }
     });
 
     socket.on('playerDied', () => {
         if (players[socket.id]) {
             players[socket.id].lives -= 1;
             if (players[socket.id].lives <= 0) {
-                players[socket.id].isDead = true;
-                socket.broadcast.emit('playerEliminated', socket.id);
+                players[socket.id].isDead = true; socket.broadcast.emit('playerEliminated', socket.id);
                 if (Object.values(players).every(p => p.isDead)) { io.emit('gameOverAll'); resetServerState(); }
             }
         }
@@ -146,15 +130,13 @@ io.on('connection', (socket) => {
         for (const [key, item] of Object.entries(gameState.items)) {
             if (item.holder === socket.id) { item.holder = null; item.visible = true; io.emit('itemUpdate', { itemId: key, itemState: item }); }
         }
-        delete players[socket.id];
-        io.emit('playerLeft', socket.id);
+        delete players[socket.id]; io.emit('playerLeft', socket.id);
     });
 });
 
 function resetServerState() {
-    currentPresetIndex = Math.floor(Math.random() * PRESETS.length);
     loadPreset();
-    Object.keys(players).forEach(k => { players[k].lives = 5; players[k].isDead = false; players[k].isHiding = false; });
+    Object.keys(players).forEach(k => { players[k].lives = 5; players[k].isDead = false; players[k].isHiding = false; players[k].isCrouching = false; });
     io.emit('serverReset', { gameState, presetIndex: currentPresetIndex });
 }
 
